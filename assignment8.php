ee2a6bd1b733f738550cdbc87a54d4e11c0a3dbf
bchok <bchok91@gmail.com>
2016-11-07 16:15:05 -0500

    added example of object interfaces

--- assignment8.php
+++ assignment8.php
@@ -27,5 +27,36 @@
     echo '<br>';
     $bar->printPHP();
 
+/////////////////////////////////////////////////////////////////////////////
+    
+    interface iTemplate{
+        public function setVariable($name, $var);
+        public function getHtml($template);
+    }
+
+    
+    class Template implements iTemplate{
+        private $vars = array();
+  
+        public function setVariable($name, $var){
+            $this->vars[$name] = $var;
+        }
+  
+        public function getHtml($template){
+            foreach($this->vars as $name => $value) {
+                $template = str_replace('{' . $name . '}', $value, $template);
+            }
+ 
+            return $template;
+        }
+    }
+
+    $obj = new Template;
+    echo $obj->setVariable('Brian', 'Test');
+
+    $obj2 = new Template;
+    echo '<br>';
+    echo $obj2->getHtml('google');
+
 ?>      
 
