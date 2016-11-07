57d27f624f31ee2c22ec74b4253ed3438e0cca71
bchok <bchok91@gmail.com>
2016-11-07 16:01:26 -0500

    added retyped reflection example from code.tutsplus.com

---
+++ Editor.php
@@ -0,0 +1,22 @@
+<?php
+
+    class Editor {
+ 
+        private $name;
+        public $articleId;
+ 
+        function __construct($name) {
+            $this->name = $name;
+        }
+ 
+        public function setNextArticle($articleId) {
+            $this->articleId = $articleId;
+        }
+ 
+        public function publish() {
+            // publish logic goes here
+            return true;
+        }
+ 
+    }
+?>
